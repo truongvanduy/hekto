e98b734e775ecdd168ab4cf1d7a9f418492e81ca
Duy <truongvanduy09072002@gmail.com>
2022-04-10 22:12:07 +0700

    decrease a half of container padding-inline

---
+++ public/js/test.js
@@ -0,0 +1,16 @@
+let post = [
+  {
+    title: 'title-1',
+    content: 'content-1'
+  },
+  {
+    title: 'title-1',
+    content: 'content-1'
+  },
+  {
+    title: 'title-1',
+    content: 'content-1'
+  },
+]
+
+var posts = `${post}`

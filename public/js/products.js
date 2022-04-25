1a61e08116974d8a10f5079e11f33114ea9d860b
Duy <truongvanduy09072002@gmail.com>
2022-04-25 17:23:22 +0700

    add some products to product list

--- public/js/products.js
+++ public/js/products.js
@@ -2,8 +2,43 @@ const products = [
   {
     id: 1,
     name: "Brantwood Everest Oak Steel",
-    imgSrc: "../products/brantwood-everest-oak-steel.png",
+    imgSrc: "../images/products/brantwood-everest-oak-steel.png",
     price: 1799,
     quantity: 0,
-  }
+  },
+  {
+    id: 2,
+    name: "Eboni Lamp Table",
+    imgSrc: "../images/products/eboni-lamp-table.png",
+    price: 599,
+    quantity: 0,
+  },
+  {
+    id: 3,
+    name: "Tobi End Table",
+    imgSrc: "../images/products/tobi-end-table.png",
+    price: 229,
+    quantity: 0,
+  },
+  {
+    id: 4,
+    name: "Emse Chair",
+    imgSrc: "../images/products/esme-chair.png",
+    price: 949,
+    quantity: 0,
+  },
+  {
+    id: 5,
+    name: "Buckland Ladder",
+    imgSrc: "../images/products/buckland-ladder.png",
+    price: 499,
+    quantity: 0,
+  },
+  {
+    id: 6,
+    name: "The Sofology Fifth Avenue",
+    imgSrc: "../images/products/the-sofology-fifth-avenue.png",
+    price: 3549,
+    quantity: 0,
+  },
 ]
